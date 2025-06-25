0f2b30b5ec8c073a6372383d0bef1dcfcb239fd1
deevite <ilyuha0794@gmail.com>
2025-06-25 13:44:11 +0600

    Добавлена кнопка Все отзывы

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -47,7 +47,7 @@ const Testimonials = () => {
         {/* Кнопка Все отзывы */}
         <div className="text-center mt-12">
           <a
-            href="https://t.me/a/#-1001370993888" //
+            href="https://t.me/BullTrendNews" //
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block px-6 py-3 bg-blue-600 text-white rounded-xl font-semibold hover:bg-blue-500 transition"
